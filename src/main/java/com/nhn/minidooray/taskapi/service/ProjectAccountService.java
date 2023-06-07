3ad207dbb9a7eaadfa2b96724fc441269e892c93
kimhyunjun <rlaguswns94@naver.com>
2023-06-07 20:48:05 +0900

    사용자의 프로젝트 권한 가져오기 (단건) 작업 중...

--- src/main/java/com/nhn/minidooray/taskapi/service/ProjectAccountService.java
+++ src/main/java/com/nhn/minidooray/taskapi/service/ProjectAccountService.java
@@ -3,8 +3,7 @@ package com.nhn.minidooray.taskapi.service;
 import com.nhn.minidooray.taskapi.domain.request.ProjectAccountCreateRequest;
 import com.nhn.minidooray.taskapi.domain.request.ProjectAccountUpdateRequest;
 import com.nhn.minidooray.taskapi.domain.response.AccountByProjectResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.nhn.minidooray.taskapi.domain.response.ProjectAccountRequest;
 
 import java.util.List;
 
@@ -15,5 +14,7 @@ public interface ProjectAccountService {
 
     void deleteProjectAccount(Long projectId, String accountId);
 
-    Page<AccountByProjectResponse> getAccountsByProject(Long projectId, Pageable pageable);
+    List<AccountByProjectResponse> getAccountsByProject(Long projectId);
+
+    ProjectAccountRequest getAccountByProject(Long projectId, String accountId);
 }
